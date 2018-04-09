1c6b5588233c2298caeda881075bd64e5cda691f
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-04-09 12:57:45 +0200

    added get transactions route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,18 @@
 // ========================================
 module.exports = (app, publicPath) => {
     const path = require('path');
+    const isAuthenticated = (req,res,next) => req.isAuthenticated() ? next() : res.redirect('/');
+    
+    app.use('/api', isAuthenticated);
+    app.get('/api/transactions', (req, res) => {
+        // app.locals.models.Transactions.findAndCountAll({
+        app.locals.models.Transactions.findAll({
+            where: { user_id: req.user.id },
+            include: ['category']
+        }).then( (transactions) => {
+            res.json(transactions);
+        });
+    });
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(publicPath, 'index.html'));

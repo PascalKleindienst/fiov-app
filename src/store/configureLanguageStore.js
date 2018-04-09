0629c9a4130f1bf5b432c3b0a177eef6b57fe3b7
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-04-09 19:40:55 +0200

    set locale for numeral

--- src/store/configureLanguageStore.js
+++ src/store/configureLanguageStore.js
@@ -3,12 +3,15 @@
 // ==============================================
 import { initialize, addTranslation, setActiveLanguage } from 'react-localize-redux';
 import globalTranslations from '../languages/global.json';
+import numeral from 'numeral';
+import 'numeral/locales/de';
 
 // Init Languages
 export default (store) => {
     store.dispatch(initialize(['en', 'de']));
     store.dispatch(addTranslation(globalTranslations, { defaultLanguage: 'en' }));
     store.dispatch(setActiveLanguage('de'));
+    numeral.locale('de');
 
     return store;
 };

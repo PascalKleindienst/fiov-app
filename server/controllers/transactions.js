0c3a12334df8fe41724b4fd86084b7cd45a03b11
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-04-22 11:18:48 +0200

    added delete and update controller methods + sequelize mock

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -4,15 +4,12 @@
 
 // List all transactions of the current user.
 module.exports.list_all_transactions = (req, res) => {
-    req.app.locals.models.Transactions.findAll({ //findAndCountAll
+    return req.app.locals.models.Transactions.findAll({ //findAndCountAll
         where: { user_id: req.user.id },
         include: ['category']
     })
         .then( (transactions) => {
-            res.json(transactions);
-        })
-        .catch((err) => {
-            res.send(err);
+            return res.json(transactions);
         });
 };
 
@@ -20,12 +17,26 @@ module.exports.list_all_transactions = (req, res) => {
 module.exports.create_transaction = (req, res) => {
     const { description = '', note = '', amount = 0, createdAt = null } = req.body;
 
-    req.app.locals.models.Transactions
+    return req.app.locals.models.Transactions
         .create({ description, note, amount, createdAt, user_id: req.user.id })
         .then((transaction) => {
-            res.json(transaction.get());
-        })
-        .catch((err) => {
-            res.send(err);
+            return res.json(transaction.get());
         });
+};
+
+// Delete transaction for the current user.
+module.exports.delete_transaction = (req, res) => {
+    return req.app.locals.models.Transactions
+        .destroy({ where: { id: req.params.id, user_id: req.user.id }})
+        .then(() => res.json(true));
+};
+
+// Update for the current user.
+module.exports.update_transaction = (req, res) => {
+    const fields = ['description', 'note', 'amount', 'createdAt'];
+    const where = { id: req.params.id, user_id: req.user.id };
+
+    return req.app.locals.models.Transactions
+        .update(req.body, { fields, where })
+        .then(() => res.json(true));
 };
--- tests/server/controllers/transactions.test.js
+++ tests/server/controllers/transactions.test.js
@@ -2,64 +2,83 @@
 // ROUTE TEST =============================
 // ========================================
 import transactions from '../../client/fixtures/transactions';
+const SequelizeMock = require('sequelize-mock');
 const request = require('supertest');
-const helpers = require('../../helpers');
 const controller = require('../../../server/controllers/transactions');
+const dbMock = new SequelizeMock();
 
 describe('my transactions controller', () => {
-    let req, res;
-    const rejectCallback = (done) => {
-        return jest.fn(() => {
-            expect(res.send).toHaveBeenCalledWith('error');
-            done();
-        });
-    };
+    let req, res, TransactionMock;
 
     beforeEach(() => {
+        transactions.map((transaction) => transaction.updatedAt = transaction.createdAt);
+
+        TransactionMock = dbMock.define('Transactions');
+        TransactionMock.$queueResult([
+            TransactionMock.build(transactions[0]),
+            TransactionMock.build(transactions[1]),
+            TransactionMock.build(transactions[2])
+        ]);
+
         req = {
             user: { id: 1 },
             body: {},
-            app: { locals: { models: {}  } }
+            app: { locals: { models: { Transactions: TransactionMock }  } }
         };
-        res = {};
+        res = { json: jest.fn((data) => JSON.parse(JSON.stringify(data))) };
     });
 
     test('should return transactions of a user', (done) => {
-        res.json = jest.fn(() => {
-            expect(res.json).toHaveBeenCalledWith(transactions);
-            done();
-        });
-
-        req.app.locals.models.Transactions = { findAll: helpers.resolvePromise(transactions) };
-
-        controller.list_all_transactions(req, res);
+        controller.list_all_transactions(req, res)
+            .then((json) => {
+                expect(json).toEqual(transactions);
+                done();
+            })
+            .catch(done);
     });
 
-    test('should return error message on failure', (done) => {
-        res.send = rejectCallback(done);
-        req.app.locals.models.Transactions = { findAll: helpers.rejectPromise('error') };
-
-        controller.list_all_transactions(req, res);
+    test('should create transaction for a user with default values', (done) => {
+        controller.create_transaction(req, res)
+            .then((json) => {
+                expect(json.description).toBe('');
+                expect(json.amount).toBe(0);
+                expect(json.note).toBe('');
+                expect(json.user_id).toBe(req.user.id);
+                done();
+            })
+            .catch(done);
     });
 
     test('should create transaction for a user', (done) => {
         req.body = { description: 'Some Description', amount: 1 };
-        res.json = jest.fn(() => {
-            expect(res.json).toHaveBeenCalledWith({ ...req.body, note: '', user_id: req.user.id });
-            done();
-        });
 
-        req.app.locals.models.Transactions = {
-            create: helpers.resolvePromise({ get: () => ({ ...req.body, note: '', user_id: req.user.id }) })
-        };
-
-        controller.create_transaction(req, res);
+        controller.create_transaction(req, res)
+            .then((json) => {
+                expect(json.description).toBe(req.body.description);
+                expect(json.amount).toBe(req.body.amount);
+                expect(json.user_id).toBe(req.user.id);
+                done();
+            })
+            .catch(done);
     });
 
-    test('should return error message for creating a transaction', (done) => {
-        res.send = rejectCallback(done);
-        req.app.locals.models.Transactions = { create: helpers.rejectPromise('error') };
+    test('should delete a transaction of a user', (done) => {
+        req.params = { id: 1 };
+        controller.delete_transaction(req, res)
+            .then((json) => {
+                expect(json).toBe(true);
+                done();
+            })
+            .catch(done);
+    });
 
-        controller.create_transaction(req, res);
+    test('should update a transaction of a user', (done) => {
+        req.params = { id: 1 };
+        controller.update_transaction(req, res)
+            .then((json) => {
+                expect(json).toBe(true);
+                done();
+            })
+            .catch(done);
     });
 });
